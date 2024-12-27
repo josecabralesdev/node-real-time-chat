8811ead14cbc949c8898164225a216e01e677390
josecabralesdev <josecabralesdev@gmail.com>
2024-12-27 10:30:49 -0500

    c1

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import logger from 'morgan'
+
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
+const port = process.env.PORT ?? 3000
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server, {
+  connectionStateRecovery: {}
+})
+
+io.on('connection', (socket) => {
+  console.log('A user connected')
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected')
+  })
+
+  socket.on('chat message', (message) => {
+    io.emit('chat message', message)
+  })
+})
+
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
+})
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
